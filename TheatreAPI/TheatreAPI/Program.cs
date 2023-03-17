d57c05937d8223efcf706d037da5d9df13da72f9
RazvanMacovei10 <macoveirazvan55@yahoo.com>
2023-03-17 17:19:33 +0200

    Update Program.cs

--- TheatreAPI/TheatreAPI/Program.cs
+++ TheatreAPI/TheatreAPI/Program.cs
@@ -1,14 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using TheatreAPI.BusinessLogic;
+using TheatreAPI.IBusinessLogic;
 using TheatreAPI.Models;
+using TheatreAPI.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<UserContext>(options =>
+builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+//builder.Services.AddScoped<IUserBL, UserBL>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
