8d89a0d7a670c4776961e7cf12e537ad0365c3d9
IvanRares <93336771+IvanRares@users.noreply.github.com>
2023-03-30 16:38:08 +0300

    Added logout for admin

--- TheatreWEB/src/app/admin-page/admin-default/admin-default.component.ts
+++ TheatreWEB/src/app/admin-page/admin-default/admin-default.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-admin-default',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-default.component.scss']
 })
 export class AdminDefaultComponent implements OnInit {
-
-  constructor() { }
+  isLoggedIn$:Observable<boolean>=new Observable<boolean>()
+  
+  constructor(private accountService:AccountService,private router:Router) { }
 
   ngOnInit(): void {
+    this.isLoggedIn$ = this.accountService.currentUser$.pipe(
+      map(user => !!user)
+    );
+  }
+
+  logout(){
+    this.accountService.logout();
+    this.router.navigate(['/auth']);
   }
 
 }
