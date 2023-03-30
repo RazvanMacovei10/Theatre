42e98b1acfd10bf35a0788eded276b2e51b7e912
RazvanMacovei10 <macoveirazvan55@yahoo.com>
2023-03-30 11:49:22 +0300

    Refactored

--- TheatreWEB/src/app/admin-page/theatre-register-forms/theatre-register-forms.component.ts
+++ TheatreWEB/src/app/admin-page/theatre-register-forms/theatre-register-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { RegisterForm } from '../register-form';
+import { RegisterForm } from '../../_models/register-form';
 
 @Component({
   selector: 'app-theatre-register-forms',
