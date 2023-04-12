9eb55d60b53a841d1e29f073d2356bffb8c18e08
IvanRares <93336771+IvanRares@users.noreply.github.com>
2023-04-12 17:01:19 +0300

    Added 2 new components

--- TheatreWEB/src/app/theatre-page/theatre-page-routing.module.ts
+++ TheatreWEB/src/app/theatre-page/theatre-page-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { combineLatest } from 'rxjs';
+import { AddEventComponent } from './add-event/add-event.component';
 
-const routes: Routes = [{path:"",component:HomeComponent}];
+const routes: Routes = [
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'schedule/add', component: AddEventComponent},
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class TheatrePageRoutingModule { }
+export class TheatrePageRoutingModule {}
--- TheatreWEB/src/app/theatre-page/theatre-page.module.ts
+++ TheatreWEB/src/app/theatre-page/theatre-page.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { TheatrePageRoutingModule } from './theatre-page-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { AddEventComponent } from './add-event/add-event.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ScheduleComponent,
+    AddEventComponent
   ],
   imports: [
     CommonModule,
