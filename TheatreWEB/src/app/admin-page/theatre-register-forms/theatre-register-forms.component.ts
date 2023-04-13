68bcdfce3b27bdd8fa95a5596c957cb645cf9276
RazvanMacovei10 <macoveirazvan55@yahoo.com>
2023-04-13 11:04:22 +0300

    Added image for each theatre form register

--- TheatreWEB/src/app/admin-page/theatre-register-forms/theatre-register-forms.component.ts
+++ TheatreWEB/src/app/admin-page/theatre-register-forms/theatre-register-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { RegisterForm } from '../../_models/register-form';
 import { RegisterFormsService } from 'src/app/_services/register-forms.service';
@@ -29,47 +29,11 @@ export class TheatreRegisterFormsComponent implements OnInit {
     private sanitizer: DomSanitizer,
     private registerFormService: RegisterFormsService
   ) {
-    fetch('assets/default-theatre.jpg')
-      .then((response) => response.blob())
-      .then((blob) => {
-        const file = new File([blob], 'default-theatre.jpg', {
-          type: 'image/jpeg',
-        });
-        this.fileToByteArray(file).subscribe((byteArray) => {
-          this.registerForm.image = window.btoa(
-            String.fromCharCode(...byteArray)
-          );
-        });
-      });
+
   }
 
   ngOnInit(): void {}
 
-  byteArrayToImageUrl(): any {
-    const encoder = new TextEncoder();
-    const binaryString = window.atob(this.registerForm.image);
-    const bytes = new Uint8Array(binaryString.length);
-    for (let i = 0; i < binaryString.length; i++) {
-      bytes[i] = binaryString.charCodeAt(i);
-    }
-    const blob = new Blob([bytes], {
-      type: 'image/jpeg',
-    });
-    const safeUrl = URL.createObjectURL(blob);
-    return this.sanitizer.bypassSecurityTrustUrl(safeUrl);
-  }
-
-  fileToByteArray(file: File): Observable<Uint8Array> {
-    return new Observable((observer) => {
-      const reader = new FileReader();
-      reader.onload = () => {
-        const array = new Uint8Array(reader.result as ArrayBuffer);
-        observer.next(array);
-        observer.complete();
-      };
-      reader.readAsArrayBuffer(file);
-    });
-  }
   approveTheatre() {
     this.registerFormService
       .createUser(this.registerForm.id.toString())
@@ -79,5 +43,16 @@ export class TheatreRegisterFormsComponent implements OnInit {
     this.registerFormService
       .deleteForm(this.registerForm.id.toString())
       .subscribe(() => this.reloadList.emit());
+  }
+
+  getImageUrl(): SafeUrl {
+
+    const imageDataBytes = new Uint8Array(atob(this.registerForm.image).split('').map(char => char.charCodeAt(0)));
+    
+
+    const base64ImageData = btoa(String.fromCharCode(...imageDataBytes));
+    
+
+    return this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + base64ImageData);
   }
 }
